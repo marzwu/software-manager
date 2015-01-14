e5f83aabac54f8dc524c9bfcdbd1b2c823d4a8c9
marzwu <marzwu@gmail.com>
2015-01-15 00:02:07 +0800

    add swt test

---
+++ src/com/marz/Program.java
@@ -0,0 +1,28 @@
+package com.marz;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setText("hello world");
+		Button btn = new Button(shell, SWT.PUSH);
+		btn.setText("btn");
+		btn.setSize(60, 32);
+		btn.setLocation(100, 100);
+		shell.open();
+		
+		while(!shell.isDisposed()){
+			if(display.readAndDispatch() == false){
+				display.sleep();
+			}
+		}
+		shell.dispose();
+	}
+
+}
